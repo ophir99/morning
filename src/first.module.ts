cf8ec1d9bf59802f9f4a831cff621d0c5b84ad27
kavya.pl <kavya.pl@gmail.com>
2019-08-15 07:09:48 +0530

    Child components, Data binding and event binding

--- src/first.module.ts
+++ src/first.module.ts
@@ -5,9 +5,16 @@ import { ThirdModule } from "./third.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { FirstComponent } from "./first/first.component";
 import { SecondComponent } from "./second/second.component";
+import { ThirdComponent } from "./app/third/third.component";
+import { FourthComponent } from "./app/fourth/fourth.component";
 
 @NgModule({
-  declarations: [FirstComponent, SecondComponent],
+  declarations: [
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent
+  ],
   imports: [SecondModule, ThirdModule, BrowserModule],
   bootstrap: [SecondComponent]
 })
--- src/second/second.component.ts
+++ src/second/second.component.ts
@@ -2,6 +2,32 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-second-component",
-  templateUrl: "./second.component.html"
+  templateUrl: "./second.component.html",
+  styleUrls: ["./second.component.css"]
 })
-export class SecondComponent {}
+export class SecondComponent {
+  name = "Angular";
+  age = 10;
+  language = "ENG";
+
+  giveText(x) {
+    let wish = "Good morning";
+    const time = "09:00";
+    wish = `Good afternoon ${this.name}`;
+
+    return `Hi ${wish}  the time is ${time} O' clock by ${x}`;
+    // return "Hi " + wish + " the time is " + time + " O clock by " + x;
+  }
+
+  respond() {
+    alert(`Hi ${this.name}`);
+  }
+
+  inc() {
+    this.age = this.age + 1;
+  }
+
+  dec() {
+    this.age = this.age - 1;
+  }
+}
