20487fa321dfbae878005379c546bda48b88ec7c
kavya.pl <kavya.pl@gmail.com>
2019-08-29 06:49:47 +0530

    forms

--- src/first.module.ts
+++ src/first.module.ts
@@ -4,19 +4,12 @@ import { ThirdModule } from "./third.module";
 
 import { BrowserModule } from "@angular/platform-browser";
 import { FirstComponent } from "./first/first.component";
-import { SecondComponent } from "./second/second.component";
-import { ThirdComponent } from "./app/third/third.component";
-import { FourthComponent } from "./app/fourth/fourth.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    FirstComponent,
-    SecondComponent,
-    ThirdComponent,
-    FourthComponent
-  ],
-  imports: [SecondModule, ThirdModule, BrowserModule],
-  bootstrap: [SecondComponent]
+  declarations: [FirstComponent],
+  imports: [SecondModule, ThirdModule, BrowserModule, FormsModule],
+  bootstrap: [FirstComponent]
 })
 export class FirstModule {
   constructor() {
--- src/first/first.component.ts
+++ src/first/first.component.ts
@@ -4,4 +4,27 @@ import { Component } from "@angular/core";
   selector: "app-first-component",
   templateUrl: "./first.component.html"
 })
-export class FirstComponent {}
+export class FirstComponent {
+  // language: string = "";
+  // year: string;
+  // creator: string;
+  // bindLanguage(event) {
+  //   console.log(event);
+  //   this.language = event.target.value;
+  // }
+
+  // bindLanguage(dom) {
+  //   console.log(dom);
+  //   this.language = dom.value;
+  // }
+
+  language = {
+    name: "",
+    year: "",
+    creator: ""
+  };
+
+  logData() {
+    console.log(this);
+  }
+}
