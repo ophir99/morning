5e659005da73fa524ae3fcabb585214dd405a762
kavya.pl <kavya.pl@gmail.com>
2019-09-19 06:33:57 +0530

    http

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -5,9 +5,24 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class PostService {
+  BASEURL = "http://localhost:4567/posts";
   constructor(private http: HttpClient) {}
 
   create(data) {
-    return this.http.post("http://localhost:4567/posts/new", data);
+    return this.http.post(`${this.BASEURL}/new`, data);
   }
+
+  getAll() {
+    return this.http.get(`${this.BASEURL}/getall`);
+  }
+
+  delete(id) {
+    return this.http.delete(`${this.BASEURL}/delete/${id}`);
+  }
+
+  // update(id, post) {
+  //   return this.http.put(`${this.BASEURL}/${id}`, post);
+  // }
+
+  update = (id, post) => this.http.put(`${this.BASEURL}/${id}`, post);
 }
--- src/second/second.component.ts
+++ src/second/second.component.ts
@@ -9,12 +9,21 @@ import { PostService } from "src/app/post.service";
 })
 export class SecondComponent {
   postForm: FormGroup;
+  editPost: FormGroup;
   msg = "";
+  msg_ = "";
+  postId = "";
+  posts = [];
   constructor(private postService: PostService) {
     this.postForm = new FormGroup({
       title: new FormControl(),
       description: new FormControl()
     });
+    this.editPost = new FormGroup({
+      title: new FormControl(),
+      description: new FormControl()
+    });
+    this.getPosts();
   }
 
   create() {
@@ -35,6 +44,57 @@ export class SecondComponent {
       () => {
         setTimeout(() => {
           this.msg = "";
+        }, 4000);
+      }
+    );
+  }
+
+  getPosts() {
+    this.postService.getAll().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.posts = data;
+        console.log(this.posts);
+      },
+      err => console.log(err)
+    );
+  }
+
+  delete(id) {
+    this.postService.delete(id).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  edit(post) {
+    console.log(post);
+    this.editPost.patchValue(post);
+    this.postId = post._id;
+  }
+
+  update() {
+    console.log(this.editPost.value);
+    this.msg_ = "Updating Data....";
+    this.postService.update(this.postId, this.editPost.value).subscribe(
+      res => {
+        console.log(res);
+        setTimeout(() => {
+          this.editPost.reset();
+          this.msg_ = "Data updated successfully";
+        }, 2000);
+      },
+      err => {
+        console.log(err);
+        this.msg_ = "Data failed to update. Try again :(";
+      },
+      () => {
+        setTimeout(() => {
+          this.msg_ = "";
         }, 4000);
       }
     );
